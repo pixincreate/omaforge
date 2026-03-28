008139fe749840023ef97c46e76d55d49ab06910
PiX <69745008+pixincreate@users.noreply.github.com>
2026-03-29 01:22:07 +0530

    refactor: rename repos/ to external/
    Avoids confusion with existing repositories/ directory.

--- unix/fedora/install/external/all.sh
+++ unix/fedora/install/external/all.sh
@@ -3,6 +3,6 @@ set -eEuo pipefail
 
 log_section "External Repositories"
 
-run_logged "$OMAFORGE_COMMON_INSTALL/repos/skillset.sh"
+run_logged "$OMAFORGE_COMMON_INSTALL/external/skillset.sh"
 
 log_success "External repositories configured"
